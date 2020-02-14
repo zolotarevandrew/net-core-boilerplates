56ae12e28f9f075a9112e98794cc1e2c37909f74
Андрей <zolotarev@ckr-sota.ru>
2020-02-14 20:04:47 +0300

    BinaryInsertionSorting

---
+++ net.algorithms/src/Algorithms/Sortings/BinaryInsertionSorting.cs
@@ -0,0 +1,31 @@
+﻿using Algorithms.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Sortings
+{
+    public static class BinaryInsertionSorting
+    {
+        public static IEnumerable<T> Apply<T>(IEnumerable<T> source, Func<T, T, bool> moreThanComparer)
+        {
+            return Apply(source.ToArray(), moreThanComparer);
+        }
+
+        public static T[] Apply<T>(T[] source, Func<T, T, bool> moreThanComparer)
+        {
+            for (int idx = 1; idx < source.Length; idx++)
+            {
+                T currentItem = source[idx];
+                int prev = idx - 1;
+                while(prev >= 0 && moreThanComparer(source[prev], currentItem))
+                {
+                    source[prev + 1] = source[prev];
+                    prev--;
+                }
+                source[prev + 1] = currentItem;
+            }
+            return source;
+        }
+    }
+}
