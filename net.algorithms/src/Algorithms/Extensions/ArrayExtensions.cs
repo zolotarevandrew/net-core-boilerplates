c0de746aa61f2a01ced6b68d9a7a58cdffe7ac2d
Андрей <zolotarev@ckr-sota.ru>
2020-02-14 17:38:19 +0300

    улучшенная BubbleSort

---
+++ net.algorithms/src/Algorithms/Extensions/ArrayExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Algorithms.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static void Swap<T>(this T[] a, int i1, int i2)
+        {
+            T t = a[i1];
+            a[i1] = a[i2];
+            a[i2] = t;
+        }
+    }
+}
---
+++ net.algorithms/src/Algorithms/Sortings/BubbleSort.cs
@@ -0,0 +1,37 @@
+﻿using Algorithms.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Sortings
+{
+    public class BubbleSort
+    {
+        public static IEnumerable<T> Apply<T>(IEnumerable<T> source, Func<T, T, bool> moreThanComparer)
+        {
+            return Apply(source.ToArray(), moreThanComparer);
+        }
+
+        public static T[] Apply<T>(T[] source, Func<T,T, bool> moreThanComparer)
+        {
+            bool isSorted = false;
+            int lastUnsorted = source.Length - 1;
+            while(!isSorted)
+            {
+                isSorted = true;
+                for (int idx = 0; idx < lastUnsorted; idx++)
+                {
+                    var item1 = source[idx];
+                    var item2 = source[idx + 1];
+                    if (moreThanComparer(item1, item2))
+                    {
+                        source.Swap(idx, idx+1);
+                        isSorted = false;
+                    }
+                }
+                lastUnsorted--;
+            }
+            return source;
+        }
+    }
+}
