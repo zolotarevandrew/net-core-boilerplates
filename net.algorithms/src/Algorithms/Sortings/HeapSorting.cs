e37bf4fef3eee2a34a7ef97387397ec714eb74a2
Андрей <zolotarev@ckr-sota.ru>
2020-02-19 20:36:22 +0300

    HeapSort

---
+++ net.algorithms/src/Algorithms/Sortings/HeapSorting.cs
@@ -0,0 +1,73 @@
+﻿using Algorithms.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Sortings
+{
+    public static class HeapSorting
+    {
+        public static IEnumerable<T> HeapSort<T>(this IEnumerable<T> source, Func<T, T, bool> moreThanComparer)
+        {
+            return HeapSort(source.ToArray(), moreThanComparer);
+        }
+
+        public static T[] HeapSort<T>(this T[] source, Func<T, T, bool> moreThanComparer)
+        {
+            int length = source.Length;
+
+            var index = length / 2 - 1;
+            while (index >= 0)
+            {
+                source.BubbleDown(length, index, moreThanComparer);
+                index--;
+            }
+
+            var end = length - 1;
+
+            while (end >= 0)
+            {
+                source.Swap(0, end);
+                source.BubbleDown(end, 0, moreThanComparer);
+                end--;
+            }
+
+            return source;
+        }
+
+        public static void BubbleDown<T>(this T[] source, int length, int index, Func<T, T, bool> moreThanComparer)
+        {
+            int curIdx = index;
+            while(curIdx < length)
+            {
+                int leftIndex = LeftIndex(curIdx);
+                if (leftIndex >= length) return;
+
+                int rightIndex = RightIndex(curIdx);
+                int maxChildIdx = leftIndex;
+                if (rightIndex < length && moreThanComparer(source[rightIndex], source[leftIndex]))
+                {
+                    maxChildIdx = rightIndex;
+                }
+
+                if (moreThanComparer(source[maxChildIdx], source[curIdx]))
+                {
+                    source.Swap(curIdx, maxChildIdx);
+                    curIdx = maxChildIdx;
+                    continue;
+                }
+                return;
+            }
+        }
+
+        static int LeftIndex(int parent)
+        {
+            return 2 * parent + 1;
+        }
+
+        static int RightIndex(int parent)
+        {
+            return 2 * parent + 2;
+        }
+    }
+}
---
+++ net.algorithms/tests/Algorithms.Tests/Sortings/HeapSortingTest.cs
@@ -0,0 +1,45 @@
+﻿using Algorithms.Extensions;
+using Algorithms.Sortings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Algorithms.Tests.Sortings
+{
+    public class HeapSortingTest
+    {
+        [Fact]
+        public void SortedData_Apply_ShouldReturnSortedData()
+        {
+            //Arrange
+            var data = new List<int>
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9
+            };
+
+            //Act
+            var res = data.HeapSort((it1, it2) => it1 > it2);
+
+            //Assert
+            Assert.True(data.SequenceEqual(res));
+        }
+
+        [Fact]
+        public void UnsortedData_Apply_ShouldReturnSortedData()
+        {
+            //Arrange
+            var data = new List<int>
+            {
+                13, 3, 8, 1, 15, 2, 3, 7
+            };
+
+            //Act
+            var res = data.HeapSort((it1, it2) => it1 > it2);
+
+            //Assert
+            data.Sort();
+            Assert.True(data.SequenceEqual(res));
+        }
+    }
+}
